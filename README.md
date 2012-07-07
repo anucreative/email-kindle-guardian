9d0e723c8de39fcd371989ea4e6dfe9b7a0076bd
Robert Douglas <rob@ribot.co.uk>
2012-07-08 00:14:53 +0100

    Moving email addresses and password to config.js

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Email Kindle Guardian
 This is a simple node programme to download they day's *.mobi copy of the Guardian from mythic-beasts.com (Mark did most of the work: http://mythic-beasts.com/~mark/random/guardian-for-kindle/) and send it to a Kindle email address over email.
 
-Built and tested with node 0.8.
+Built and tested with node 0.6.
 
 It's run as a node-cron job to send you a copy of the paper each morning. I have mine to send to run at 05:00.
 
 ## Set up
-Requires python Mailer which can be found at http://pypi.python.org/pypi/mailer
+Requires:
+- nodemailer
+- moment
+- fs
+- url
+- http
+- cron
 
-In the config.ini file, change:
-    FromEmail to an email address that is authorised to send Kindle books to your Kindle.
-    KindleEmail to your Kindle's free or paid email account.
+Create a config.js file with the following:
+var config = {
+	kindleEmail: "Your kindle email address",
+	fromEmail: "Your GMail address",
+	fromPassword: "Your GMail password"
+};
+
+module.exports = config;
--- app.js
+++ app.js
@@ -1,90 +1,95 @@
-var nodemailer = require("nodemailer");
-var moment = require("moment");
-var fs = require("fs");
-var url = require('url');
-var http = require('http');
-var cronJob = require('cron').CronJob;
+var
 
-var downloadDir = "./tmp/";
-var formattedDate;
+// Required modules
+nodemailer = require("nodemailer"),
+moment = require("moment"),
+fs = require("fs"),
+url = require("url"),
+http = require("http"),
+cronJob = require("cron").CronJob,
+config = require("./config"),
 
 
-var sendGuardian = function(fileName) {
-    fs.readFile(downloadDir + fileName, function(err, data) {
-        if (err) {
-            console.log(err);
-            return;
+sendGuardian = function(filePath) {
+    fs.readFile(filePath, function(err, data) {
+        if (err) console.log(err);
+        else {
+
+            // Create reusable transport method (opens pool of SMTP connections)
+            var smtpTransport = nodemailer.createTransport("SMTP",{
+                service: "Gmail",
+                auth: {
+                    user: config.fromEmail,
+                    pass: config.fromPassword
+                }
+            });
+
+            // Setup e-mail data
+            var mailOptions = {
+                from: config.fromEmail,
+                to: config.kindleEmail,
+                subject: "Guardian",
+                attachments: [{
+                    filename: filePath.split("/").pop(),
+                    contents: data
+                }]
+            };
+
+            // Send mail with defined transport object
+            smtpTransport.sendMail(mailOptions, function(err, response){
+                if (err) console.log(error);
+                else {
+                    console.log("Message sent: " + response.message);
+                    // TODO: Remove .mobi file as we don"t need it anymore
+                }
+
+                // Shut down the connection pool, no more messages
+                smtpTransport.close();
+            });
         }
-
-        // create reusable transport method (opens pool of SMTP connections)
-        var smtpTransport = nodemailer.createTransport("SMTP",{
-            service: "Gmail",
-            auth: {
-                user: "rob@anucreative.com",
-                pass: "olsen570"
-            }
-        });
-
-        // Setup e-mail data
-        var mailOptions = {
-            from: "Robert Douglas <rob@anucreative.com>",
-            to: "anucreative@kindle.com",
-            subject: "Guardian (" + formattedDate + ")",
-            attachments: [{
-                filename: fileName,
-                contents: data
-            }]
-        };
-
-        // send mail with defined transport object
-        smtpTransport.sendMail(mailOptions, function(err, response){
-            if (err) console.log(error);
-            else {
-                console.log("Message sent: " + response.message);
-                // TODO: Remove .mobi file as we don't need it anymore
-            }
-
-            smtpTransport.close(); // shut down the connection pool, no more messages
-        });
     });
-};
+},
 
 
-var downloadGuardian = function() {
+downloadGuardian = function() {
     var
     now = new moment(),
+    formattedDate,
+    downloadDir = "./tmp/",
     fileName,
-    fileUrl;
+    fileUrl,
+    options,
+    file;
     
     formattedDate = now.format("YYYY-MM-DD");
     fileName = "gdn-" + formattedDate + ".mobi";
     fileUrl = "http://mythic-beasts.com/~mark/random/guardian-for-kindle/" + fileName;
 
-    var options = {
+    options = {
         host: url.parse(fileUrl).host,
         port: 80,
         path: url.parse(fileUrl).pathname
     };
 
 
-    var file = fs.createWriteStream(downloadDir + fileName);
+    file = fs.createWriteStream(downloadDir + fileName);
 
     http.get(options, function(res) {
-        res.on('data', function(data) {
+        res.on("data", function(data) {
             file.write(data);
-        }).on('end', function() {
+        }).on("end", function() {
             file.end();
-            console.log(fileName + ' downloaded to ' + downloadDir);
-            sendGuardian(fileName);
+            console.log(fileName + " downloaded to " + downloadDir);
+            sendGuardian(downloadDir + fileName);
         });
     });
-};
+},
 
 
-var job = new cronJob({
-    cronTime: '00 00 5 * * *',
+job = new cronJob({
+    cronTime: "00 00 5 * * *",
     onTick: function(){
-        console.log('Wakey wakey, time to download the Guardian');
+        console.log("Wakey wakey, time to download the Guardian");
         downloadGuardian();
     },
     start: false
